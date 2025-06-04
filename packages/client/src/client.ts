4426b8a66a59b64029cc5d7c5cd8fa76a63af32c
matheussatoshi1 <matheussdev3@gmail.com>
2025-06-04 08:49:53 -0400

    chore: bump @iocx/client version to 0.1.5 and @iocx/server version to 0.1.3, refactor createIOCXClient to createRPCXClient

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -1,6 +1,10 @@
 import { IOCX } from "@iocx/server";
 
-export function createIOCXClient<R extends IOCX<any>>(router: R) {
+export function createRPCXClient<R extends IOCX<any>>(
+  router: R,
+): {
+  call: ReturnType<typeof IOCX.__extractContext>;
+} {
   const ctx = IOCX.__extractContext(router);
   return {
     call: ctx,
